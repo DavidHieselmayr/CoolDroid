0f27c0d1c2d5e6a6e3da4ac82f24f719bcac4d3a
David <unbekannt.ghost@gmail.com>
2021-11-08 10:02:38 +0100

    finish

--- app/src/main/java/com/example/cooldroid/MainActivity.kt
+++ app/src/main/java/com/example/cooldroid/MainActivity.kt
@@ -6,25 +6,34 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val COUNTER_KEY = "COUNTER"
     internal val LOG_TAG = MainActivity::class.java.simpleName
     var counter = 0
-    //lateinit var tvMessage: TextView
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER_KEY, counter)
+        super.onSaveInstanceState(outState)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // tvMessage = findViewById(R.id.textView)
-       // tvMessage.text = "Old way"
+        if(savedInstanceState!=null) {
+            counter = savedInstanceState.getInt(COUNTER_KEY)
+            tvCoolDroid.text = getMessageText()
+        }
         ivCoolDroid.setOnClickListener{
             counter++
-            Log.d(LOG_TAG, "onCreate(), counter:${counter}")
-            tvCoolDroid.text =
-                when (counter) {
-                    1 -> getString(R.string.once)
-                    2 -> getString(R.string.twice)
-                    else -> getString(R.string.text_view_numbers, counter)
-                }
+            Log.d(LOG_TAG, "onCreate(), counter: ${counter}")
+            tvCoolDroid.text = getMessageText()
+
         }
-
-
     }
+
+    private fun getMessageText() =
+        when (counter) {
+            1 -> getString(R.string.once)
+            2 -> getString(R.string.twice)
+            else -> getString(R.string.text_view_numbers, counter)
+        }
 }
